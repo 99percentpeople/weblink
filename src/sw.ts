dc5f42456f88df619411c4cee642c4a741e0f94a
YZZ <myzzzyearz@outlook.com>
2024-10-16 20:05:21 +0800

    cache i18n jsons

--- src/sw.ts
+++ src/sw.ts
@@ -7,12 +7,20 @@ import {
   NavigationRoute,
   registerRoute,
 } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { ExpirationPlugin } from "workbox-expiration";
+
 declare let self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
+  // send notification to user
+  self.registration.showNotification("Weblink", {
+    body: "Weblink is ready to use",
+  });
 });
 
 self.addEventListener("message", (event) => {
@@ -23,16 +31,28 @@ self.addEventListener("message", (event) => {
 // clean old assets
 cleanupOutdatedCaches();
 
-// Cache i18n jsons
-registerRoute(
-  new NavigationRoute(
-    createHandlerBoundToURL("i18n/*.json"),
-  ),
-);
-
 // to allow work offline
 registerRoute(
   new NavigationRoute(
     createHandlerBoundToURL("index.html"),
   ),
+);
+
+// 缓存 i18n JSON 文件
+registerRoute(
+  ({ request }) =>
+    request.url.includes("/i18n/") &&
+    request.url.endsWith(".json"),
+  new CacheFirst({
+    cacheName: "i18n-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 10, // 最多缓存10个文件
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30天后过期
+      }),
+    ],
+  }),
 );
