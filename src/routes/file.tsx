4454a732bfc6e1e6be3d39aca5dd92a13e4d8da6
Zach <myzzzyearz@outlook.com>
2024-10-22 16:55:53 +0800

    add data table sort function

--- src/routes/file.tsx
+++ src/routes/file.tsx
@@ -235,6 +235,7 @@ export default function File() {
     }),
     columnHelper.display({
       id: "actions",
+      header: () => <div class="w-9" />,
       cell: ({ row }) => (
         <>
           <DropdownMenu>
